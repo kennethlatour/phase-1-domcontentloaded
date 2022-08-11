f57fd12fc8cb1d15795d0f311406411a337dfa24
Kenneth Latour <kenneth.latourii@gmail.com>
2022-08-11 09:37:40 -0500

    commit tree 8dbf224ff72f96e8635a1bd48ebe3b3b15902ec3

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function () {
+  let p = document.getElementById('text')
+  p.textContent = 'This is really cool!'
+})
+// function chan(tex){
+//   let p = document.getElementById('text')
+//   p.innerContent = "This is really cool!"
+// }
+//chan(e.target.text)
